afdbb653e05ebbaa6472475f5f70fbf443c7657c
GaboJohn <johngabotsalelwe@gmail.com>
2024-03-09 06:57:49 -0800

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ from os.path import exists
 
 env.hosts = ['54.88.187.237', '52.91.121.185']
 
+
 def do_deploy(archive_path):
     """Deploy archive to web servers"""
     if not exists(archive_path):
@@ -16,12 +17,12 @@ def do_deploy(archive_path):
 
     try:
         file_x = archive_path.split("/")[-1]
-        exile = file_x.split(".")[0]  # Fix the variable name
+        exile = file_x.split(".")[0]
         path = '/data/web_static/releases/'
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, exile))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_x, path, exile))  # Remove extra '/'
-        run('rm /tmp/{}'.format(file_x))  # Remove extra '/'
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_x, path, exile))
+        run('rm /tmp/{}'.format(file_x))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, exile))
         # Remove the web_static directory
         run('rm -rf {0}{1}/web_static'.format(path, exile))
@@ -31,5 +32,6 @@ def do_deploy(archive_path):
         run('ln -s {0}{1}/ /data/web_static/current'.format(path, exile))
         print('New version deployed!')
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
